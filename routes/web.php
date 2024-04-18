f45ee0878f474187fbc671ff78e53c2428dc61f0
Antonio Cahun <cahunantonio1@gmail.com>
2024-04-18 19:40:35 +0200

    refactor(routes): improve routes structure

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,18 @@ use App\Http\Controllers\CardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('card/check');
+    return view ('welcome');
 });
 
-Route::post('/', [
-    CardController::class, 'index'
-]);
+Route::get('/cards/search', function () {
+    return view('cards/search');
+});
+
+Route::post('/cards/search', [
+    CardController::class, 'find'
+]); 
+
+Route::get('/cards/{card:slug}', [
+    CardController::class, 'show'
+])->name('cards.show');
+
